09e75f3c1b7597f4517686022d4f2f1dad578cb4
spacecat <library4lion@gmail.com>
2023-10-04 00:57:52 -0700

    initial commit

---
+++ focused-window-name.py
@@ -0,0 +1,60 @@
+import argparse
+import sys
+from i3ipc import Connection, Event
+
+parser = argparse.ArgumentParser(
+    description="Returns the given property of the currently focused window"
+)
+parser.add_argument(
+    "-o",
+    "--output_length",
+    type=int,
+    help="max number of characters to print for title",
+)
+parser.add_argument(
+    "prop",
+    choices=["class", "instance", "name", "title"],
+    type=str,
+    help="print class of focused window",
+)
+
+args = parser.parse_args()
+prop = args.prop
+output_length = args.output_length
+
+i3 = Connection()
+
+
+def get_focused_window_prop(prop: str):
+    focused = i3.get_tree().find_focused()
+    output = ""
+    if prop == "name":
+        output = focused.name
+    elif prop == "class":
+        output = focused.window_class
+    elif prop == "instance":
+        output = focused.window_instance
+    elif prop == "title":
+        output = focused.window_title
+
+    return output
+
+
+def on_window(i3: Connection, e: Event):
+    focused = i3.get_tree().find_focused()
+    if e.change == "focus" or e.change == "move" or e.change == "title":
+        output = get_focused_window_prop(prop)
+        if output_length is None:
+            print(output, flush=True)
+        else:
+            print(output[:output_length], flush=True)
+
+
+i3.on("window", on_window)
+
+try:
+    print(get_focused_window_prop(prop), flush=True)
+    i3.main()
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
---
+++ readme.md
@@ -0,0 +1,50 @@
+## motivation
+default `xwindow` module for polybar outputs full title of application which can be long and miss the name of the application (such as occurs with kitty)
+
+solution: use other properties such as window_class or window_instance
+
+**comparison: title | class**
+
+![Alt text](comparison.png)
+
+
+`class` will just print the application name
+
+`name` will print the whole title
+example:
+
+class: Code
+
+name: readme.md - i3-scripts - Visual Studio Code
+
+## installation
+```
+git clone 
+```
+
+## how to use
+```
+usage: focused-window-name.py [-h] [-o OUTPUT_LENGTH] {class,instance,name,title}
+
+Returns the title or app name of the currently focused window
+
+positional arguments:
+  {class,instance,name,title}
+                        print class of focused window
+
+options:
+  -h, --help            show this help message and exit
+  -o OUTPUT_LENGTH, --output_length OUTPUT_LENGTH
+                        max number of characters to print for title
+```
+
+
+put the following in your polybar `config.ini` with the given cli argument (example below uses `class`)
+```ini
+[module/window-name]
+type = custom/script
+label = %output%
+label-foreground = ${colors.primary}
+exec = python ~/.config/polybar/scripts/focused-window-name.py class
+tail = true
+```
