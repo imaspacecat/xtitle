6b9494cd1eb0233fdc80a911a33a4a9200e6fc53
spacecat <library4lion@gmail.com>
2023-10-07 19:06:00 -0700

    fix: format

--- window-name.py
+++ window-name.py
@@ -50,12 +50,12 @@ disp = Display()
 root = disp.screen().root
 
 # Prepare the property names we use so they can be fed into X11 APIs
-NET_ACTIVE_WINDOW = disp.intern_atom('_NET_ACTIVE_WINDOW')
-NET_WM_NAME = disp.intern_atom('_NET_WM_NAME')  # UTF-8
-WM_NAME = disp.intern_atom('WM_NAME')           # Legacy encoding
-WM_CLASS = disp.intern_atom('WM_CLASS')
+NET_ACTIVE_WINDOW = disp.intern_atom("_NET_ACTIVE_WINDOW")
+NET_WM_NAME = disp.intern_atom("_NET_WM_NAME")  # UTF-8
+WM_NAME = disp.intern_atom("WM_NAME")  # Legacy encoding
+WM_CLASS = disp.intern_atom("WM_CLASS")
 
-last_seen = {'xid': None, 'title': None, 'class': None}  # type: Dict[str, Any]
+last_seen = {"xid": None, "title": None, "class": None}  # type: Dict[str, Any]
 
 
 @contextmanager
@@ -64,7 +64,7 @@ def window_obj(win_id: Optional[int]) -> Window:
     window_obj = None
     if win_id:
         try:
-            window_obj = disp.create_resource_object('window', win_id)
+            window_obj = disp.create_resource_object("window", win_id)
         except XError:
             pass
     yield window_obj
@@ -72,19 +72,18 @@ def window_obj(win_id: Optional[int]) -> Window:
 
 def get_active_window() -> Tuple[Optional[int], bool]:
     """Return a (window_obj, focus_has_changed) tuple for the active window."""
-    response = root.get_full_property(NET_ACTIVE_WINDOW,
-                                      X.AnyPropertyType)
+    response = root.get_full_property(NET_ACTIVE_WINDOW, X.AnyPropertyType)
     if not response:
         return None, False
     win_id = response.value[0]
 
-    focus_changed = (win_id != last_seen['xid'])
+    focus_changed = win_id != last_seen["xid"]
     if focus_changed:
-        with window_obj(last_seen['xid']) as old_win:
+        with window_obj(last_seen["xid"]) as old_win:
             if old_win:
                 old_win.change_attributes(event_mask=X.NoEventMask)
 
-        last_seen['xid'] = win_id
+        last_seen["xid"] = win_id
         with window_obj(win_id) as new_win:
             if new_win:
                 new_win.change_attributes(event_mask=X.PropertyChangeMask)
@@ -105,7 +104,7 @@ def _get_window_name_inner(win_obj: Window) -> str:
                 if isinstance(win_name, bytes):
                     # Apparently COMPOUND_TEXT is so arcane that this is how
                     # tools like xprop deal with receiving it these days
-                    win_name = win_name.decode('latin1', 'replace')
+                    win_name = win_name.decode("latin1", "replace")
                 return win_name
             else:
                 title = "<unnamed window>"
@@ -116,8 +115,8 @@ def _get_window_name_inner(win_obj: Window) -> str:
 def get_window_name(win_id: Optional[int]) -> Tuple[Optional[str], bool]:
     """Look up the window name for a given X11 window ID"""
     if not win_id:
-        last_seen['title'] = None
-        return last_seen['title'], True
+        last_seen["title"] = None
+        return last_seen["title"], True
 
     title_changed = False
     with window_obj(win_id) as wobj:
@@ -127,22 +126,23 @@ def get_window_name(win_id: Optional[int]) -> Tuple[Optional[str], bool]:
             except XError:
                 pass
             else:
-                title_changed = (win_title != last_seen['title'])
-                last_seen['title'] = win_title
+                title_changed = win_title != last_seen["title"]
+                last_seen["title"] = win_title
 
-    return last_seen['title'], title_changed
+    return last_seen["title"], title_changed
+
 
 def get_window_class(win_id: Optional[int]) -> Optional[str]:
     if not win_id:
-        last_seen['class'] = "Nice wallpaper!"
-        return last_seen['class']
-    
+        last_seen["class"] = "Nice wallpaper!"
+        return last_seen["class"]
+
     with window_obj(win_id) as wobj:
         if wobj:
             wm_class = wobj.get_wm_class()[1]
-            last_seen['class'] = wm_class
-    
-    return last_seen['class']
+            last_seen["class"] = wm_class
+
+    return last_seen["class"]
 
 
 def handle_xevent(event: Event):
@@ -153,10 +153,10 @@ def handle_xevent(event: Event):
     changed = False
     if event.atom == NET_ACTIVE_WINDOW:
         if get_active_window()[1]:
-            get_window_name(last_seen['xid'])  # Rely on the side-effects
+            get_window_name(last_seen["xid"])  # Rely on the side-effects
             changed = True
     elif event.atom in (NET_WM_NAME, WM_NAME):
-        changed = changed or get_window_name(last_seen['xid'])[1]
+        changed = changed or get_window_name(last_seen["xid"])[1]
 
     if changed:
         handle_change(last_seen)
@@ -164,10 +164,13 @@ def handle_xevent(event: Event):
 
 def handle_change(new_state: dict):
     """Replace this with whatever you want to actually do"""
-    print(get_window_class(new_state['xid']).capitalize(), flush=True) # flush=True so polybar functions properly
+    print(
+        get_window_class(new_state["xid"]).capitalize(), flush=True
+    )  # flush=True so polybar functions properly
     # print(new_state)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Listen for _NET_ACTIVE_WINDOW changes
     root.change_attributes(event_mask=X.PropertyChangeMask)
 
